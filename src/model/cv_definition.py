dd591ee05a8e6966d78b22f449857cd8b2ee8f37
Thomas Davies <tom.davies@voquis.com>
2025-10-16 22:06:35 +0100

    CV-2 changed yaml and optional skills
    changed yamel to not use hythens and soley rely on indentations and
    makes the skill definition optional

--- src/model/cv_definition.py
+++ src/model/cv_definition.py
@@ -32,4 +32,5 @@ class CvDefinition:
     employment_layout: str = ExperienceLayout.PROJECT
     independent_projects: list[IndependentProject] | None = None
     awarding_bodies: list[AwardingBody] | None = None
-    skills: list[dict[str, str]] | None = None
+    skills: dict[str, str] | None = None
+
